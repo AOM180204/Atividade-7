ad8694493304a3fc140dd2f9e7b40a2e382a24f7
AOM180204 <147025492+AOM180204@users.noreply.github.com>
2024-04-13 16:35:32 -0300

    Main.java

--- Atividade 7/Atividade 7-2/Atividade72/src/Main.java
+++ Atividade 7/Atividade 7-2/Atividade72/src/Main.java
@@ -1,7 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Data data1= new Data();
-        Data data2= new Data();
+        String str=JOptionPane.showInputDialog("Insira a data no formato DD/MM/AAAA");
+        String[] data=str.split("/");
+        String Dia=data[0];
+        String mês=data[1];
+        String ano=data[2];
+        Data data1= new Data(Dia, mês, ano);
+        Data data2= new Data("12","12","2023");
         data1.getDia();
         data1.getMes();
         data1.getAno();
