09002b339d0e369fe5a1882143668e81c0e0da2b
AOM180204 <147025492+AOM180204@users.noreply.github.com>
2024-04-13 17:08:53 -0300

    Update Main.java

--- Atividade 7/Atividade 7-3/Atividade73/src/Main.java
+++ Atividade 7/Atividade 7-3/Atividade73/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Hora hora1= new Hora(23, 44, 30);
-        Hora hora2= new Hora(23, 40, 30);
+        String str=JOptionPane.showInputDialog("Insira a hora no formato HH:MM:SS");
+        String[] hora=str.split(":");
+        String Hora=hora[0];
+        String Minuto=hora[1];
+        String Segundo=hora[2];
+        Hora hora1= new Hora(Hora, Minuto, Segundo);
+        Hora hora2= new Hora("23", "40", "30");
         hora1.getHoras();
         hora1.getMinutos();
         hora1.getSegundos();
@@ -9,3 +14,4 @@ public class Main {
         hora1.displayHora();
     }
 }
+
